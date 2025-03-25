bc591b1978e31fa46e00cb45e2a0c20c75c5f295
paklong <paklong2556@gmail.com>
2025-03-24 21:59:19 -0700

    feat: add login form and logo to application
    - Implemented LoginForm component with username/password inputs and submit button using Tailwind CSS
    - Added Logo component displaying Icon-192.png in App.jsx
    - Updated index.html with inline background color (#F3F8D0)
    - Removed unused src/assets/react.svg

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,64 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-7xl">hi</h1>
-    </>
+    <div className="">
+      <Logo />
+      <div className="flex justify-center">
+        <LoginForm />
+      </div>
+    </div>
   );
 }
 
+function Logo() {
+  return (
+    <div className="flex justify-center mt-80">
+      <img src="/Icon-192.png" alt="ArtWink Studio Logo"></img>
+    </div>
+  );
+}
+
+function LoginForm() {
+  const handleSubmit = () => {
+    console.log("Login");
+  };
+  return (
+    <form>
+      <div className="flex flex-col gap-3">
+        <div className="flex justify-between flex-wrap">
+          <label className="color-gray-500 text-xs" htmlFor="username">
+            Username:
+          </label>
+          <input
+            className="border-1 border-gray-400 w-full rounded-lg"
+            id="username"
+            type="text"
+            autoComplete="username"
+            required
+          />
+        </div>
+        <div className="flex justify-between flex-wrap">
+          <label className="color-gray-500 text-xs" htmlFor="password">
+            Password:
+          </label>
+          <input
+            className="border-1 border-gray-400 w-full rounded-lg"
+            id="password"
+            type="password"
+            autoComplete="new-password"
+            required
+          />
+        </div>
+        <div className="flex justify-center flex-wrap">
+          <button
+            className="border-1 border-gray-400 rounded-lg text-sm w-1/2 self-center mt-1"
+            onClick={handleSubmit}
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
 export default App;
